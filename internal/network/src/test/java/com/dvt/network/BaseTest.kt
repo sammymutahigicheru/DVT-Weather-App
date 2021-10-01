08b06609b10af9ed67d15bb0e0914f4a5ea0f717
Sammy Mutahi <sammymutahi12@gmail.com>
2021-10-02 00:10:51 +0300

    add weather request dispatcher and base test class that mocks making calls to apiservices

---
+++ internal/network/src/test/java/com/dvt/network/BaseTest.kt
@@ -0,0 +1,55 @@
+package com.dvt.network
+
+import com.dvt.network.api.WeatherAPI
+import com.dvt.network.commons.WeatherRequestDispatcher
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+internal open class BaseTest {
+
+    private lateinit var mockWebServer: MockWebServer
+
+    lateinit var weatherApiService: WeatherAPI
+
+    private lateinit var okHttpClient: OkHttpClient
+
+    private lateinit var loggingInterceptor: HttpLoggingInterceptor
+
+    @Before
+    open fun setup() {
+        mockWebServer = MockWebServer()
+        mockWebServer.dispatcher = WeatherRequestDispatcher()
+        mockWebServer.start()
+        loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        okHttpClient = buildOkhttpClient(loggingInterceptor)
+
+        weatherApiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(WeatherAPI::class.java)
+    }
+
+    @After
+    open fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    private fun buildOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+}
