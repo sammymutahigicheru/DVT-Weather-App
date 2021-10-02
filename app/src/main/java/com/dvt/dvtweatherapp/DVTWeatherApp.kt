634f63ef9732e7957f9dd3ff4efc3a78fe515210
Sammy Mutahi <sammymutahi12@gmail.com>
2021-10-02 13:40:49 +0300

    add timber for debugging

--- app/src/main/java/com/dvt/dvtweatherapp/DVTWeatherApp.kt
+++ app/src/main/java/com/dvt/dvtweatherapp/DVTWeatherApp.kt
@@ -9,12 +9,19 @@ import org.koin.core.context.startKoin
 import org.koin.core.error.KoinAppAlreadyStartedException
 import org.koin.core.logger.Level
 import org.koin.core.module.Module
+import timber.log.Timber
 
 class DVTWeatherApp:Application() {
     override fun onCreate() {
         super.onCreate()
-
+        initTimber()
         initilizeKoin()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     private fun initilizeKoin() {
