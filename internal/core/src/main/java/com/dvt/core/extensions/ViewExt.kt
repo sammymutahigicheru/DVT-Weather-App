a0758c86e7d73a7b9d42e3321260471e4b8eb32d
Sammy Mutahi <sammymutahi12@gmail.com>
2021-10-03 16:27:44 +0300

    add save favorites functionality

---
+++ internal/core/src/main/java/com/dvt/core/extensions/ViewExt.kt
@@ -0,0 +1,17 @@
+package com.dvt.core.extensions
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+fun View.showSuccessSnackbar(message: String) {
+    val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+
+    snackbar.apply {
+        this.setBackgroundTint(
+            ContextCompat.getColor(view.context, android.R.color.white)
+        )
+        this.setTextColor(ContextCompat.getColor(this.context, android.R.color.black))
+        show()
+    }
+}
